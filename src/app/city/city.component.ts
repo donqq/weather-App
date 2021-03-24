9a722b1daeb9affdb0aa3aec7183f94836294300
Don Dilanga <bld@dilanga.com>
2021-03-25 02:16:57 +0530

    Updated the copyright notice

--- src/app/city/city.component.ts
+++ src/app/city/city.component.ts
@@ -1,3 +1,12 @@
+/* Copyright (C) 2021/March Badde Liyanage Don Dilanga ( github@dilanga.com ) - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the Simple non code license (SNCL)
+ * https://tldrlegal.com/license/simple-non-code-license-(sncl)
+ * You should have received a copy of the Simple non code license (SNCL) with
+ * this file. If not, please write to: github@dilanga.com ,
+ * or visit : https://github.com/donqq/weather-App
+ */
+
 import { Component, Input, OnInit } from '@angular/core';
 import { cityColours } from '../data';
 import { WeatherDetails, CityColour, WeatherIcon } from '../weatherInterface';
